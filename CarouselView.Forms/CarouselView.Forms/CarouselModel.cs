8fb7b2f4d716adfb34ea3662cb8d34810be48631
Luis David De la cruz <delacruz_luis@outlook.es>
2018-03-01 13:57:16 -0600

    Xamarin.Forms Carousel View

---
+++ CarouselView.Forms/CarouselView.Forms/CarouselModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarouselView.Forms
+{
+    public class CarouselModel
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ CarouselView.Forms/CarouselView.Forms/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarouselView.Forms
+{
+	public partial class MainPage : ContentPage
+	{
+        public List<CarouselModel> MyDataSource { get; set; }
+        private int _position;
+        public int Position { get { return _position; } set { _position = value; OnPropertyChanged(); } }
+        public MainPage()
+		{
+			InitializeComponent();
+            MyDataSource = new List<CarouselModel>() { new CarouselModel() { Title = "XamSarp", Detail="Contenido de Xamarin.Forms" },
+                                                        new CarouselModel() { Title = "Sigue el ", Detail="Recibe correos electrónicos" },
+                                                        new CarouselModel() { Title = "Blog", Detail="Luis David" }};
+
+            BindingContext = this;
+        }
+	}
+}
